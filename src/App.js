89a46cabe06b9f0626b01ee1fb40c931dc70bb96
Rajdev Yadav <ryindia2023@gmail.com>
2023-05-28 13:37:41 -0400

    LocalStorage and Delete funtionality added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,50 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [todos, setTodos]= useState([]);
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
   const [todo, setTodo] = useState('');
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
-  function handleInputChange(e){
-   setTodo(e.target.value)  ;
+  function handleInputChange(e) {
+    setTodo(e.target.value);
   };
 
-  function handleFormSubmit(e){
+  function handleFormSubmit(e) {
     e.preventDefault();
 
-    if(todo !== ''){
-      setTodos([...todos, {id: todos.length = 1, task: todo.trim()}])
+    if (todo !== '') {
+      setTodos([...todos, { id: todos.length + 1, task: todo.trim() }])
     }
     setTodo('');
+  }
+
+  function handleDelete(id) {
+    const removeTodo = todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    setTodos(removeTodo);
+    console.log(removeTodo)
   }
   return (
     <div className="App">
       <div className="todo">
         <ul className="todo-list">
-          <li>this is todo item</li>
+          <h2>Have A Great Day!</h2>
           {
-            todos.map((todo)=>{ return <li key={todo.id}>{todo.task}</li>})
+            todos.map((todo) => { return <li key={todo.id}>{todo.task} <button onClick={() => { handleDelete(todo.id) }}>Delete</button> </li> })
           }
         </ul>
         <form className="todo-form" onSubmit={handleFormSubmit}>

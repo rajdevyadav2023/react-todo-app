6f001c4e1d9844b943100c71700c38bce47bcea2
Rajdev Yadav <ryindia2023@gmail.com>
2023-05-28 12:56:20 -0400

    Todo and Todos system built successfully , now it's turn to store todos item in localstorage so that when user refresh it should be remain.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos, setTodos]= useState([]);
+  const [todo, setTodo] = useState('');
+
+
+  function handleInputChange(e){
+   setTodo(e.target.value)  ;
+  };
+
+  function handleFormSubmit(e){
+    e.preventDefault();
+
+    if(todo !== ''){
+      setTodos([...todos, {id: todos.length = 1, task: todo.trim()}])
+    }
+    setTodo('');
+  }
   return (
     <div className="App">
-     <h1>this is app componetns</h1>
+      <div className="todo">
+        <ul className="todo-list">
+          <li>this is todo item</li>
+          {
+            todos.map((todo)=>{ return <li key={todo.id}>{todo.task}</li>})
+          }
+        </ul>
+        <form className="todo-form" onSubmit={handleFormSubmit}>
+          <p>React Todo App By Rajdev Yadav</p>
+          <div className="form-control">
+            <input type="text" value={todo} placeholder='Enter Task Here & Press Enter To Add' onChange={handleInputChange} />
+
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
